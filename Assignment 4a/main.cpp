8f16405854fdc503d168d29726b2df8ed8d2b610
reuben <reuben@campus-049-156.ucdavis.edu>
2016-10-17 10:44:38 -0700

    Initial Commit

---
+++ Assignment 4a/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4a
+//
+//  Created by reuben on 10/17/16.
+//  Copyright (c) 2016 reuben. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
